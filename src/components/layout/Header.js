d636733eb4839291669ec51d77cbc945356077f5
Rafeh Qazi <qazi@cleverprogrammer.com>
2021-01-04 20:20:40 -0800

    trying to add my own svg.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,7 @@
 import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
-
-const menuData = [
-  { title: "Courses", icon: "images/icons/courses.svg", link: "/courses" },
-  { title: "Tutorials", icon: "images/icons/tutorials.svg", link: "/courses" },
-  {
-    title: "Pricing",
-    icon: "images/icons/pricing.svg",
-    link: "/pricing",
-  },
-]
+import { menuData } from "../../data/menuData"
 
 export default function Header() {
   return (
@@ -19,14 +10,14 @@ export default function Header() {
       <MenuWrapper>
         {menuData.map((item, index) => (
           <Link to={item.link} key={index}>
-            <MenuItem>
+            <MenuItem title={item.title}>
               <img src={item.icon} alt={item.title} />
               {item.title}
             </MenuItem>
           </Link>
         ))}
       </MenuWrapper>
-    </Wrapper>
+    </Wrapper>  
   )
 }
 
@@ -44,14 +35,22 @@ const Wrapper = styled.div`
 const MenuWrapper = styled.div`
   display: grid;
   gap: 30px;
-  grid-template-columns: repeat(3, auto);
+  grid-template-columns: repeat(${menuData.length}, auto);
 `
 
 const MenuItem = styled.div`
   color: rgba(255, 255, 255, 0.7);
   display: grid;
   grid-template-columns: 24px auto;
-  gap: 10px;
+  gap: ${props => (props.title ? "30px" : "0px")};
   align-items: center;
   padding: 10px;
+  transition: 0.1s ease-out;
+  border-radius: 10px;
+
+  :hover {
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
+  }
 `
